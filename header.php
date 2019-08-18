bf0f9f0db8009464451b1192921acc4b94815e33
Sherman Wong <s3656264@student.rmit.edu.au>
2019-08-18 15:37:12 +1000

    Added logo, made responsive menu

--- header.php
+++ header.php
@@ -8,24 +8,37 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="wireframe.css">
-	<div class="logo"><img src="logo.png" /></div>
-	<div class="nav"><nav>
+	<img class="logo" src="logo.png" max-width="100%"/>
+	<nav class="topnav" id="mytopnav">
 		<a href="index.php">Home</a>
 		<a href="menu.php">About Us</a>
 		<a href="order.php">How to Use</a>
 		<a href="cart.php">Pricing</a>
 		<a href="queue.php">Locations and Cars</a>
-	</nav></div>
+		<a href="javascript:void(0);" class="icon" onclick="openmenu()">
+		<i class="fa fa-bars"></i>
+		</a>
+	</nav>
 	<div class="login">
 		<?php 
 			if(isset($_SESSION['user'])){
 				echo "<a href='logout.php'>Logout</a>";
 			}
 			else {
-				echo "<a href='register.php'>Login</a>";
+				echo "<a href='register.php'>Register</a>";
 				echo "<a href='login.php'>Login</a>";
 			}
 		?>
 	</div>
-
+	<script>
+		function openmenu() {
+			var x = document.getElementById("Topnav");
+			if (x.className === "topnav") {
+				x.className += " responsive";
+			}
+			celse {
+				x.className = "topnav";
+			}
+		}
+	</script>
 </head>
