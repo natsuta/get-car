d8146feafa9e5c358bf20c681f529fea58160db5
Sherman Wong <s3656264@student.rmit.edu.au>
2019-08-26 13:36:38 +1000

    Added Bootstrap code to header

--- header.php
+++ header.php
@@ -10,6 +10,14 @@
 	<link rel="stylesheet" type="text/css" href="wireframe.css">
 	<!-- This is for the menu icon -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- This is for Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
 	<div class="logocontainer"><img class="logo" src="logo.png"/></div>
 	<div class="topnav" id="mytopnav">
