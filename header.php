e2c02adf8beeafe1ba420a9d50e30fb504b06a8a
Sherman Wong <s3656264@student.rmit.edu.au>
2019-09-01 23:46:35 +1000

    Testing Javascript to detect active page

--- header.php
+++ header.php
@@ -67,4 +67,13 @@
 			</ul>
 		</div>
 	</nav>
+
+	<script>
+		$(document).ready(function() {
+			// get current URL path and assign 'active' class
+			var pathname = window.location.pathname;
+			$('.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+		})
+	</script>
+
 </head>
