916b9d79a30232a079d4f4d2a7b287f669cae705
Sherman Wong <s3656264@student.rmit.edu.au>
2019-09-11 11:02:10 +1000

    Added support for hashing passwords in database

--- login_process.php
+++ login_process.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $servername = "104.154.78.63";
 $username = "root";
 $password = "123456";
@@ -8,19 +10,25 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error); 
-}else{
+}
+else {
 
-      $Email = $_POST["Email"];
-      $Password = $_POST["Password"];
+	$Email = $_POST["Email"];
+	$Password = $_POST["Password"];
 
-      $sql = "select * from customers where email = '$Email' and password='$Password'";
-      $result = $conn->query($sql);
-      $row=mysqli_fetch_array($result);
-      if ($row) {
-      echo"You are now logged in.";
-    } else {
-      echo "Error! Something wrong in your username or password!";
-    }
+	$sql = "select * from customers where email = '$Email' and password='$Password'";
+	$result = $conn->query($sql);
+	$row=mysqli_fetch_array($result);
+	if (password_verify($Password, $row['password'])) {
+		$_SESSION['email'] = $row['email'];
+		$_SESSION['firstName'] = $row['firstName'];
+		$_SESSION['lastName'] = $row['lastName'];
+
+		echo"You are now logged in.";
+	} 
+	else {
+		echo "Error! Something wrong in your username or password!";
+	}
 }
 
 ?>
--- register_process.php
+++ register_process.php
@@ -8,42 +8,49 @@ $conn = new mysqli($servername, $username, $password, $dbname);
  
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error); 
-} else{
-      
-      $FirstName = $_POST['FirstName'];
-      $LastName = $_POST['LastName'];
-      $Mobile = $_POST['Mobile'];
-      $Email = $_POST['Email'];
-      $Password = $_POST['Password'];
-      $RePassword = $_POST['RePassword'];
+}
+else {
+	$FirstName = $_POST['FirstName'];
+	$LastName = $_POST['LastName'];
+	$Mobile = $_POST['Mobile'];
+	$Email = $_POST['Email'];
+	$UserType = "Customer";
+	$Password = $_POST['Password'];
+	$RePassword = $_POST['RePassword'];
 
-     if($Password != $RePassword){
-      echo"Password does not match";
-      }else{
+	if($Password != $RePassword) {
+		echo"Password does not match";
+	}
+		
+	else {
+		//hash the password
+		$PasswordHash = hash($Password, PASSWORD_DEFAULT);
+		
+		//connect to database and see if email exists
+		$sql="select * from customers where email = '$Email'";
+		$result = $conn->query($sql);
+		$row=mysqli_fetch_array($result);
 
-       $sql="select * from customers where email = '$Email'";
-       $result = $conn->query($sql);
-       $row=mysqli_fetch_array($result);
+		if($row) {
+		echo "Email already exists";
+		}
 
-        if($row){
+		else {
 
-          echo"Email already exists";
+			$query = "insert into customers(email, firstName, lastName, mobile, userType, password) 
+				VALUES('$Email', '$FirstName', '$LastName', '$Mobile', '$UserType', '$PasswordHash')";      
+			$result=$conn->query($query);
+		
+			if($result) {
+				mysqli_commit($conn);
+				mysqli_close($conn);
+				echo "Registered successful!";
+			}
 
-        }else{
-
-      $query = "insert into customers(email, firstName, lastName, mobile, password) VALUES('$Email', '$FirstName', '$LastName', '$Mobile', '$Password')";      
-      $reslut=$conn->query($query);
-     
-     if($result){
-      echo "Registered successful!";          
-      } 
-      else{
-        echo "Registered failed!"; 
-      }    
-    }
-      
-  }
-  
-}    
-
+			else {
+				echo "Registered failed!"; 
+			}
+		}
+	}
+}
 ?>
