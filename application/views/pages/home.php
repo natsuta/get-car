198dac0d7b45e76d4cf0eeb554556fc1ec9146c2
Sherman Wong <s3656264@student.rmit.edu.au>
2019-09-19 08:42:10 +1000

    Updated home page from master branch

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,7 +5,7 @@
 	}
 
 	.hero-image {
-		background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("getcarbanner.jpg");
+		background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("assets/imgs/getcarbanner.jpg");
 		height: 50%;
 		background-position: center;
 		background-repeat: no-repeat;
@@ -46,20 +46,23 @@
 		<div class="hero-text">
 			<h1 style="font-size:50px">Welcome to GetCar</h1>
 			<p>Sign up now to use our services!</p>
-			<button onclick="window.location.href = 'customer/register.php';">Register</button>
+			<button onclick="window.location.href = 'cust_register';">Register</button>
 		</div>
 	</div>
 
 	<div class="content">
 		<center>
+			<br>
+			<h2>We ignite opportunity by setting the world in motion</h2>
 			<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultrices lorem eget ligula dictum auctor. Aliquam purus lorem, viverra vitae lorem non, sollicitudin malesuada augue. Integer consectetur ante sit amet est dignissim porttitor. Nam massa nulla, finibus scelerisque molestie nec, hendrerit in lacus. Ut aliquet mi nec risus scelerisque, quis mattis tortor scelerisque. Aenean sagittis cursus nibh vel efficitur. Donec id eros consequat ex vestibulum tempus ut vitae ex. Etiam non laoreet magna. Suspendisse vel ultrices tellus. Aliquam nec velit malesuada, elementum eros in, lacinia dui. Pellentesque sit amet lacinia sapien. Sed lobortis volutpat est at pulvinar.
+			Good things happen when people can move, whether across town or towards their dreams. Opportunities appear, open up, become reality. What started as a way to tap a button to get a ride has led to billions of moments of human connection as people around the world go all kinds of places in all kinds of ways with the help of our technology.
+			<br>
+			<h2> Rent and drive</h2>
+			<p>
+			GetCar is a carshare company that owns a number of cars parked at various locations around the city of Melbourne. The way our program works is that users will book a car for a period of time, then return it at any GetCar location available. The system incorporates a user, car and rental information database along with a web & mobile application.
 			<br>
 			<br>
-			Morbi tincidunt purus vehicula molestie cursus. Ut scelerisque tincidunt viverra. Curabitur efficitur nec dolor quis iaculis. Quisque aliquam maximus diam, non facilisis tortor iaculis at. Ut blandit accumsan tellus, non rhoncus tellus placerat ac. Suspendisse bibendum imperdiet leo eget ornare. Nulla luctus cursus urna. Fusce tempor dolor velit, non pulvinar odio mattis in. Curabitur aliquet enim eros, a lacinia mauris interdum at. Nunc et libero vitae justo luctus euismod. Suspendisse posuere est quis urna maximus vehicula. Nunc id hendrerit metus.
-			<br>
-			<br>
-			Phasellus placerat venenatis placerat. Donec bibendum massa magna, ac lobortis mauris pellentesque eu. Nulla at dapibus orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc vel metus eu mauris pellentesque dictum. Pellentesque nec ornare orci, non tincidunt velit. Phasellus vel vehicula augue. Cras tristique egestas ligula. Suspendisse feugiat, purus at tincidunt aliquet, lectus nunc tincidunt elit, id mattis magna neque vel tortor. Integer faucibus lacinia condimentum. Vestibulum imperdiet risus et tortor placerat, sit amet dictum erat cursus. Vestibulum malesuada vulputate ante sed sagittis.
+			<b>Want to learn more about us? Click <u><a href="about">here</a></u>.</b>
 			</p> 
 		</center>
 	</div>
