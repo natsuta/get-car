841b586c54f4da6075b63ce64b1fbdb18959fed9
OnlyLegendx <53281675+OnlyLegendx@users.noreply.github.com>
2019-09-27 15:22:07 +1000

    Update booking_process.php

--- application/views/pages/booking_process.php
+++ application/views/pages/booking_process.php
@@ -3,8 +3,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
-$dbname = "cars";
+$password = "getcar123456";
+$dbname = "testcars";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
  
@@ -26,19 +26,19 @@ if($pickupdate == $returndate) {
 	else{
 
 
-			$query = "insert into bookings(BookingID, Pickup_location, Return_location, Bodytype, Vehicle_selected, Pickup_date, Return_date) 
-				VALUES('null', '$pickuplocation', '$returnlocation', '$bodytype', '$car', '$pickupdate', '$returndate')";      
+			$query = "insert into Bookings(Pickup_location, Return_location, Bodytype, Vehicle_selected, Pickup_date, Return_date) 
+				VALUES('$pickuplocation', '$returnlocation', '$bodytype', '$car', '$pickupdate', '$returndate')";      
 			$result=$conn->query($query);
 		
 			if($result) {
 				mysqli_commit($conn);
-				mysqli_close($conn);
+				
 				echo "Booking added! </a>";
 			}
 			else {
 				echo "Booking failed</a>"; 
 			}
-			
+			mysqli_close($conn);
 		}
 	
 }
