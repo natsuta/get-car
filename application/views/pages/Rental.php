95db8a41c649431a15f501cf691d6af9b8cd0b0b
Sherman Wong <s3656264@student.rmit.edu.au>
2019-10-17 09:31:06 +1100

    Corrected code

--- application/views/pages/Rental.php
+++ application/views/pages/Rental.php
@@ -2,7 +2,7 @@
 	<h2>Rental Infomation</h2>
 	<div class="container">
 			
-			<table border="1" align="center" class = "table">
+		<table border="1" align="center" class = "table">
     	<tr>
         	<th align="center" width="10%">Rental ID</th>
         	<th align="center" width="10%">User ID</th>
@@ -13,56 +13,49 @@
         	<th align="center" width="10%">Cost</th>
     	</tr>
 
-				<?php
-					
-					$servername = "localhost";
-					$username = "root";
-					$password = "getcar123456";
-					$dbname = "getcar";
+		<?php
+			
+			$servername = "localhost";
+			$username = "root";
+			$password = "getcar123456";
+			$dbname = "getcar";
 
-					$conn = mysqli_connect($servername, $username, $password, $dbname);
+			$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-					if (mysqli_connect_errno()){
-  						echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  					}
+			if (mysqli_connect_errno()){
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
 
-						$sql = "SELECT * FROM rental;";
-						$result=mysqli_query($conn,$sql);
+			$sql = "SELECT * FROM rental;";
+			$result=mysqli_query($conn,$sql);
 
-						$sql2 = "SELECT * FROM customers;";
-						$result2=mysqli_query($conn,$sql);
+			$sql2 = "SELECT * FROM customers;";
+			$result2=mysqli_query($conn,$sql);
 
-						$sql3 = "SELECT * FROM cars;";
-						$result3=mysqli_query($conn,$sql);
+			$sql3 = "SELECT * FROM cars;";
+			$result3=mysqli_query($conn,$sql);
 
-						$sql4 = "SELECT * FROM locations;";
-						$result4=mysqli_query($conn,$sql);
+			$sql4 = "SELECT * FROM locations;";
+			$result4=mysqli_query($conn,$sql);
 
-						$sql5 = "SELECT * FROM rates;";
-						$result5=mysqli_query($conn,$sql);
+			$sql5 = "SELECT * FROM rates;";
+			$result5=mysqli_query($conn,$sql);
 
-						while ($row = mysqli_fetch_assoc($result)){	
-						?>
-						<div class="rental">
-
-							<?php
-							echo "<tr>";
-							echo "<td align='center'>".$row['rental_id']."</td>";
-							echo "<td align='center'>".$row['customerID']."</td>";
-							echo "<td align='center'>".$row['locationID']."</td>";
-							echo "<td align='center'>".$row['carID']."</td>";
-							echo "<td align='center'>".$row['start_date']."</td>";
-							echo "<td align='center'>".$row['end_date']."</td>";
-							echo "<td align='center'>".$row['cost']."</td>";
-				?>
-			</div>
-<?php
-
+			while ($row = mysqli_fetch_assoc($result)){	
+				echo "<tr>";
+				echo "<td align='center'>".$row['rental_id']."</td>";
+				echo "<td align='center'>".$row['customerID']."</td>";
+				echo "<td align='center'>".$row['locationID']."</td>";
+				echo "<td align='center'>".$row['carID']."</td>";
+				echo "<td align='center'>".$row['start_date']."</td>";
+				echo "<td align='center'>".$row['end_date']."</td>";
+				echo "<td align='center'>".$row['cost']."</td>";
 			echo "</tr>";
 		}
 		mysqli_close($conn);
 				     
 ?>
+	</table>
 	</div>
 	
 </body>
