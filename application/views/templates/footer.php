f10ae6548699b5575e009c1dd9b6019aae40ff4f
Sherman Wong <s3656264@student.rmit.edu.au>
2019-11-10 23:22:27 +1100

    Had to fix footer link again

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer mt-auto py-3">
 	<div class="container">
-		<a href="">Home</a>
+		<a href="<?php echo base_url();?>">Home</a>
 		<a href="about">About Us</a>
 		<a href="howto">How to Use</a>
 		<a href="cars">Cars</a>
